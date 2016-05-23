032abbbcf9da1808a82520bd9a5dcaf8ff155f11
Pedro Cabral <pedrocabral90@gmail.com>
2016-05-23 21:42:49 +0100

    add initial structure of layouts to support phones and tablets

---
+++ app/src/main/java/com/udacity/popularmovies/PopularMoviesApp.java
@@ -0,0 +1,9 @@
+package com.udacity.popularmovies;
+
+import android.app.Application;
+
+/**
+ * Created by pedro on 23-May-16.
+ */
+public class PopularMoviesApp extends Application {
+}
---
+++ app/src/main/java/com/udacity/popularmovies/fragments/DetailMoviesFragment.java
@@ -0,0 +1,9 @@
+package com.udacity.popularmovies.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by pedro on 23-May-16.
+ */
+public class DetailMoviesFragment extends Fragment {
+}
